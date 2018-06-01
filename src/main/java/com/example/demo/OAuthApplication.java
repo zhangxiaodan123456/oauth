bd25fd32a07f67b5b8109ee945ff358c97a39b93
'zxd' <zxd@zxd.com>
2018-06-01 17:04:09 +0800

    http://blog.didispace.com/spring-security-oauth2-xjf-1/

--- src/main/java/com/example/demo/OAuthApplication.java
+++ src/main/java/com/example/demo/OAuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OAuthApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(OAuthApplication.class, args);
 	}
